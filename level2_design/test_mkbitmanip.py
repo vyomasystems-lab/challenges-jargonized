1038056fe00c27891e28275e4e80f4c0f1faf57e
Preethaa J <mejpreethaa@gmail.com>
2022-07-23 13:29:55 +0000

    test update

--- level2_design/test_mkbitmanip.py
+++ level2_design/test_mkbitmanip.py
@@ -20,6 +20,59 @@ def clock_gen(signal):
         signal.value <= 1
         yield Timer(1) 
 
+"""
+@cocotb.test()
+def run_test_random(dut):
+
+    # clock
+    cocotb.fork(clock_gen(dut.CLK))
+
+    # reset
+    dut.RST_N.value <= 0
+    yield Timer(10) 
+    dut.RST_N.value <= 1
+
+          
+    ######### CTB : Modify the test to expose the bug #############
+    # input transaction
+
+    for i in range(100):
+
+        inst = random.randint(0,4294967295)
+
+        mav_putvalue_src1 = 0x5
+        mav_putvalue_src2 = 0x0
+        mav_putvalue_src3 = 0x0
+        mav_putvalue_instr = inst
+
+        #if (mav_putvalue_instr == 0x4100f0b3):
+            #continue
+
+        # expected output from the model
+        expected_mav_putvalue = bitmanip(mav_putvalue_instr, mav_putvalue_src1, mav_putvalue_src2, mav_putvalue_src3)
+
+        # driving the input transaction
+        dut.mav_putvalue_src1.value = mav_putvalue_src1
+        dut.mav_putvalue_src2.value = mav_putvalue_src2
+        dut.mav_putvalue_src3.value = mav_putvalue_src3
+        dut.EN_mav_putvalue.value = 1
+        dut.mav_putvalue_instr.value = mav_putvalue_instr
+
+        yield Timer(1) 
+
+        # obtaining the output
+        dut_output = dut.mav_putvalue.value
+
+        cocotb.log.info(f'DUT OUTPUT={hex(dut_output)}')
+        cocotb.log.info(f'EXPECTED OUTPUT={hex(expected_mav_putvalue)}')
+        
+        # comparison
+        error_message = f'Value mismatch DUT = {hex(dut_output)} does not match MODEL = {hex(expected_mav_putvalue)} for inst = {hex(inst)}'
+        
+        assert dut_output == expected_mav_putvalue, error_message
+
+"""
+
 # Sample Test
 @cocotb.test()
 def run_test_Rtype(dut):
@@ -75,4 +128,61 @@ def run_test_Rtype(dut):
                 
                 assert dut_output == expected_mav_putvalue, error_message
 
+
+"""@cocotb.test()
+def run_test_Itype(dut):
+
+    # clock
+    cocotb.fork(clock_gen(dut.CLK))
+
+    # reset
+    dut.RST_N.value <= 0
+    yield Timer(10) 
+    dut.RST_N.value <= 1
+
+    func5 = [0x00000000,0x20000000,0x40000000,0x60000000]
+    func5_1 = [0, 134217728]
+    func3 = [0x00001000, 0x00005000]
+    opcode = 0x00000093
+    imm = [random.randint(0,255)<<28 for i in range(5)]
+
+    for i in func5:
+        for j in func5_1:
+            for k in func3:
+                for val in imm:
+                
+                    ######### CTB : Modify the test to expose the bug #############
+                    # input transaction
+
+                    mav_putvalue_src1 = 0x5
+                    mav_putvalue_src2 = 0x3
+                    mav_putvalue_src3 = 0x0
+                    mav_putvalue_instr = hex(int(i+k+opcode+val)+j)
+
+                    #if (mav_putvalue_instr == 0x4100f0b3):
+                    # continue
+
+                    # expected output from the model
+                    expected_mav_putvalue = bitmanip(mav_putvalue_instr, mav_putvalue_src1, mav_putvalue_src2, mav_putvalue_src3)
+
+                    # driving the input transaction
+                    dut.mav_putvalue_src1.value = mav_putvalue_src1
+                    dut.mav_putvalue_src2.value = mav_putvalue_src2
+                    dut.mav_putvalue_src3.value = mav_putvalue_src3
+                    dut.EN_mav_putvalue.value = 1
+                    dut.mav_putvalue_instr.value = mav_putvalue_instr
+                
+                    yield Timer(1) 
+
+                    # obtaining the output
+                    dut_output = dut.mav_putvalue.value
+
+                    cocotb.log.info(f'DUT OUTPUT={hex(dut_output)}')
+                    cocotb.log.info(f'EXPECTED OUTPUT={hex(expected_mav_putvalue)}')
+                    
+                    # comparison
+                    error_message = f'Value mismatch DUT = {hex(dut_output)} does not match MODEL = {hex(expected_mav_putvalue)} for inst = {hex(i+j+k+opcode)}'
+                    
+                    assert dut_output == expected_mav_putvalue, error_message
+"""
 
