fdf72274d37f02e7db5fe4ff7cd4173c587c349f
Preethaa J <mejpreethaa@gmail.com>
2022-07-28 10:43:08 +0000

    test update

--- level3_design/test_ca_prng.py
+++ level3_design/test_ca_prng.py
@@ -15,10 +15,11 @@ from prng_compute import *
 async def run_test_random(dut):
 
     # clock
-    clock = Clock(dut.clk, 10, units="us")  # Create a 10us period clock on port clk
+    clock = Clock(dut.clk, 10, units="ns")  # Create a 10us period clock on port clk
     cocotb.start_soon(clock.start())    
 
     # reset
+    await FallingEdge(dut.clk)
     dut.reset_n.value = 0
     await FallingEdge(dut.clk)  
     dut.reset_n.value = 1
@@ -26,28 +27,34 @@ async def run_test_random(dut):
     count = 0
     err = []
     
+
+    #input values
+    
     initial = random.randint(0,4294967295)
-    load_initial = [0,0,0,1,0,0,0,0,0,0,0,1,0,0,0]
+    load_initial = [0,0,0,1,0,0,0,0,0,0,0,0,0,0,0]
     updaterule = 90
     defaultupdaterule = 30
-    load_update = [0,0,0,0,0,0,0,0,0,0,0,1,0,0,0]
-    nextpattern = [0,0,0,0,0,1,1,1,1,1,1,1,1,1,1]
-    expected = [0,0,0,initial]
+    load_update = [0,0,0,1,0,0,0,0,0,0,0,1,0,0,0]
+    nextpattern = [0,0,0,0,0,1,1,1,1,1,1,0,1,1,1]
+    expected = [0,0,0,0,initial]
+    my_update_rule = defaultupdaterule
+
+    #modelling the ca_prng module
 
     for i in range(4,15):
-        if (load_initial[i] and nextpattern[i]):
+        if (load_initial[i]):
             expected.append(initial)
         elif (nextpattern[i]):
             if load_update[i]:
-                expected.append(prng_compute(expected[i-1],updaterule))
-            else:
-                expected.append(prng_compute(expected[i-1],defaultupdaterule))
+                my_update_rule = updaterule
+            expected.append(prng_compute(expected[i],updaterule))
         else:
-            expected.append(expected[i-1])
+            expected.append(expected[i])
 
 
     for i in range(15):
 
+        print(i)
         init_pattern_data = initial
         load_init_pattern = load_initial[i]
         update_rule = updaterule
@@ -55,7 +62,8 @@ async def run_test_random(dut):
         next_pattern = nextpattern[i]
 
         # driving the input transaction
-        await FallingEdge(dut.clk)
+        
+        
         dut.init_pattern_data.value = init_pattern_data
         dut.load_init_pattern.value = load_init_pattern
         dut.update_rule.value = update_rule
@@ -65,14 +73,15 @@ async def run_test_random(dut):
         
         expected_prng_data = expected[i]
             
-        
-        
+              
         # obtaining the output
         await RisingEdge(dut.clk)
         dut_prng_data = dut.prng_data.value
-
+        
+        
         cocotb.log.info(f'DUT OUTPUT={hex(dut_prng_data)}')
         cocotb.log.info(f'EXPECTED OUTPUT={hex(expected_prng_data)}')
+        cocotb.log.info(f'DIFFERENCE = {bin(dut_prng_data ^ expected_prng_data)}') #for finding the error
         
         # comparison
         error_message = f'Value mismatch DUT = {hex(dut_prng_data)} does not match MODEL = {hex(expected_prng_data)}'
